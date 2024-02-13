ad8006ebd9a8f3193c160c781119a954e898c029
jonathan Dipita <jonathandipita@gmail.com>
2024-02-13 20:21:31 +0400

    create, read, and delete implemented on Slot

--- app/controllers/slots_controller.rb
+++ app/controllers/slots_controller.rb
@@ -1,2 +1,33 @@
 class SlotsController < ApplicationController
+
+  def index
+    return Slot.all
+  end
+
+  def new
+    @slot = Slot.new
+  end
+
+  def create
+    raise
+    @slot = Slot.new(slot_params)
+    redirect_to slots_path
+  end
+
+  def show
+    @slot = Slot.find(params[:id])
+  end
+
+  def destroy
+    @slot = Slot.find(params[:id])
+    @slot.destroy
+    redirect_to slots_path, status: :see_other
+  end
+
+
+  private
+
+  def slot_params
+    params.require(:slot).permit(:startTime, :endTime, :user_id, :status)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  delete "slots/:id", to: "slots#destroy", as: :slot_destroy
+  get "/slots", to: "slots#index", as: :slots
+  get "slots/new", to: "slots#new", as: :slots_new
+  post "slots", to: "slots#create"
+  get "slots/:id", to: "slots#show"
 end
